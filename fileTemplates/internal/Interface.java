b441c10dd6403c94f74c2e614ffc1ff9c3928a0b
lufeixia <lufeixiascyw0027@youedata.com>
2020-05-21 09:28:14 +0800

    IU-2018.2.7 <lufeixia@DESKTOP-HJB22BC Create print.xml, project.default.xml, Default copy.xml, Interface.java, yiiguxing.translation.xml, Default.xml, ignore.xml, find.xml, checkstyle-idea.xml, keymap.xml, code.style.schemes.xml, KeyPromoterXSettings.xml, github_settings.xml, mavenVersion.xml, Default.xml, debugger.renderers.xml, Default copy.xml, editor.codeinsight.xml, iterations.xml, editor.xml, markdown_navigator.shared.xml, debugger.xml, baseRefactoring.xml, ide.general.xml, git.xml, _@user_Default.icls, CodeGlance.xml, rainbow_brackets.xml, lombok-plugin.xml, other.xml, androidStudioFirstRun.xml, ui.lnf.xml, filetypes.xml, plugin_ui.xml, databaseDrivers.xml, templates.xml, laf.xml, KeyPromoterXStatistic.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, intentionSettings.xml, Bootstrap3.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * 说明
+ *
+ * @author lufeixia
+ * @date ${DATE} ${TIME}
+ * @since 1.8
+ * @version 1.0
+ */
+public interface ${NAME} {
+}
